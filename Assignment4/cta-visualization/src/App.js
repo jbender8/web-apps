c6a983c72cacee9144d3230da5beaaddd965a7ea
Jessica Bender <jessicabednder18@gmail.com>
2020-05-03 00:24:24 -0500

    got CTA data in

--- Assignment4/cta-visualization/src/App.js
+++ Assignment4/cta-visualization/src/App.js
@@ -36,17 +36,57 @@ class App extends Component {
               ],
             datasets: [
               {
-                label: 'Population',
+                label: 'Total',
                 data: [
-                  617594,
-                  181045,
-                  153060,
-                  106519,
-                  105162,
-                  95072
+                  data[0]['total'], data[1]['total'], data[2]['total'], data[3]['total'], data[4]['total'], data[5]['total'],
+                  data[6]['total'], data[7]['total'], data[8]['total'], data[9]['total'], data[10]['total'],
+                  data[11]['total'], data[12]['total'], data[13]['total'], data[14]['total'], data[15]['total'],
+                  data[16]['total'], data[17]['total'], data[18]['total'], data[19]['total'], data[20]['total'],
+                  data[21]['total'], data[22]['total'], data[23]['total'], data[24]['total'], data[25]['total'],
+                  data[26]['total'], data[27]['total'], data[28]['total'], data[29]['total'], data[30]['total']
+
                 ],
                 backgroundColor: 'rgba(75, 192, 192, 0.6)',
 
+              },
+              {
+                label: 'Rail',
+                data: [
+                  data[0]['rail'], data[1]['rail'], data[2]['rail'], data[3]['rail'], data[4]['rail'], data[5]['rail'],
+                  data[6]['rail'], data[7]['rail'], data[8]['rail'], data[9]['rail'], data[10]['rail'],
+                  data[11]['rail'], data[12]['rail'], data[13]['rail'], data[14]['rail'], data[15]['rail'],
+                  data[16]['rail'], data[17]['rail'], data[18]['rail'], data[19]['rail'], data[20]['rail'],
+                  data[21]['rail'], data[22]['rail'], data[23]['rail'], data[24]['rail'], data[25]['rail'],
+                  data[26]['rail'], data[27]['rail'], data[28]['rail'], data[29]['rail'], data[30]['rail']
+
+                ],
+                backgroundColor: 'rgba(255, 206, 86, 0.6)',
+              },
+              {
+                label: 'Bus',
+                data: [
+                  data[0]['bus'], data[1]['bus'], data[2]['bus'], data[3]['bus'], data[4]['bus'], data[5]['bus'],
+                  data[6]['bus'], data[7]['bus'], data[8]['bus'], data[9]['bus'], data[10]['bus'],
+                  data[11]['bus'], data[12]['bus'], data[13]['bus'], data[14]['bus'], data[15]['bus'],
+                  data[16]['bus'], data[17]['bus'], data[18]['bus'], data[19]['bus'], data[20]['bus'],
+                  data[21]['bus'], data[22]['bus'], data[23]['bus'], data[24]['bus'], data[25]['bus'],
+                  data[26]['bus'], data[27]['bus'], data[28]['bus'], data[29]['bus'], data[30]['bus']
+
+                ],
+                backgroundColor: 'rgba(255, 99, 132, 0.6)',
+              },
+              {
+                label: 'Paratransit',
+                data: [
+                  data[0]['paratransit'], data[1]['paratransit'], data[2]['paratransit'], data[3]['paratransit'], data[4]['paratransit'], data[5]['paratransit'],
+                  data[6]['paratransit'], data[7]['paratransit'], data[8]['paratransit'], data[9]['paratransit'], data[10]['paratransit'],
+                  data[11]['paratransit'], data[12]['paratransit'], data[13]['paratransit'], data[14]['paratransit'], data[15]['paratransit'],
+                  data[16]['paratransit'], data[17]['paratransit'], data[18]['paratransit'], data[19]['paratransit'], data[20]['paratransit'],
+                  data[21]['paratransit'], data[22]['paratransit'], data[23]['paratransit'], data[24]['paratransit'], data[25]['paratransit'],
+                  data[26]['paratransit'], data[27]['paratransit'], data[28]['paratransit'], data[29]['paratransit'], data[30]['paratransit']
+
+                ],
+                backgroundColor: 'rgba(153, 102, 255, 0.6)',
               }
             ]
           }
@@ -82,6 +122,7 @@ class App extends Component {
       <div className="chart">
         <Bar
           data={this.state.chartData}
+          height={250}
           options={{
             title: {
               display: this.props.displayTitle,
@@ -100,6 +141,9 @@ class App extends Component {
                 }
               }],
               yAxes: [{
+                ticks: {
+                  min: 40000
+                },
                 scaleLabel: {
                   display: this.props.displayYAxesLabel,
                   labelString: 'Boardings'
