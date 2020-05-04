a787805f7e24b9a2c20b843a31421c0e3ca32159
Jessica Bender <jessicabednder18@gmail.com>
2020-05-04 15:33:59 -0500

    added header

--- Assignment5/routing/src/App.js
+++ Assignment5/routing/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      Start of new app!
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className="title">
+            Jess Bender - Routing
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
     </div>
   );
 }
